2cff751485f844473f52b33692271711c255affa
Pavan Sukalkar <pavan.s@kukufm.com>
2025-04-20 00:36:19 +0530

    Added solution for two-sum in rust

--- rustic/src/0001_two_sum.rs
+++ rustic/src/0001_two_sum.rs
@@ -1,5 +1,55 @@
+struct Solution;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        
+        return Self::approach_2(&nums, target);
     }
+
+    #[warn(dead_code)]
+    pub fn approach_1(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+        let n = nums.len();
+        for i in 0..n {
+            for j in (i+1)..n{
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
+            }
+        }
+        return vec![];
+    }
+
+    pub fn approach_2(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+        let n = nums.len();
+        let sorted = {
+            let mut cloned = nums.to_vec();
+            cloned.sort();
+            cloned
+        };
+        let mut ci = 0;
+        let mut cj = n-1;
+        while ci < cj {
+            if sorted[ci] + sorted[cj] > target {
+                cj = cj - 1;
+            } else if sorted[ci] + sorted[cj] < target {
+                ci = ci + 1;
+            } else {
+                break;
+            }
+        }
+        let mut i: i32 = -1;
+        let mut j: i32 = -1;
+        for k in 0..n {
+            if nums[k] == sorted[ci] && i == -1 {
+                i = k as i32;
+            }
+            else if nums[k] == sorted[cj] && j == -1 {
+                j = k as i32;
+            }
+        }
+        return vec![i, j];
+    }
+}
+
+fn main() {
+    let result = Solution::two_sum(vec![2,11,7,15], 9);
+    println!("{:?}", result);
 }
--- rustic/src/main.rs
+++ rustic/src/main.rs
@@ -1,28 +1,9 @@
-use std::{cmp::Ordering, io};
-use rand::Rng;
+fn main(){
+    let s = String::from("Pavan");
+    greet_me(s);
+    println!("{s}");  // This will throw error without reference
+}
 
-fn main() {
-    println!("Guess the number");
-
-    let secret_num = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        println!("Input your guess");
-
-        let mut guess = String::new();
-    
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-    
-        let guess: u32 = guess.trim().parse().expect("Please provide an integer");
-    
-        println!("You guessed: {}", guess);
-    
-        match guess.cmp(&secret_num) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {println!("You win"); break;}
-        }    
-        
-    }
-    
+fn greet_me(s: String){
+    println!("Hello {s}");
 }
